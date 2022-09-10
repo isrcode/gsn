b23302abae5185f364cd6e0393f5ed1ba0a9b0af
Israel Vieira Ribeiro <israel.ribeiro@everymind.com.br>
2022-09-10 16:18:31 -0300

    Base da API

---
+++ src/test/java/br/com/gsn/gsn/GsnApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gsn.gsn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
